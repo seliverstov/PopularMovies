11bb44588678983f386a3d4eaa791c251d73329a
Alexander Seliverstov <seliverstov.a@gmail.com>
2015-10-16 13:31:39 +0300

    Remove Api Key

---
+++ app/src/main/java/com/seliverstov/popularmovies/rest/TMDBKey.java
@@ -0,0 +1,8 @@
+package com.seliverstov.popularmovies.rest;
+
+/**
+ * Created by a.g.seliverstov on 12.10.2015.
+ */
+public class TMDBKey {
+    public static final String API_KEY = "YOUR_API_KEY_MUST_BE_HERE";
+}
