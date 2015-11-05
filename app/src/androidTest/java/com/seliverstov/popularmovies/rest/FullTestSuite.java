572d406535e7633a5b095eccc081c04c90aa2197
Alexander Seliverstov <seliverstov.a@gmail.com>
2015-11-05 11:33:46 +0300

    Add test suites

---
+++ app/src/androidTest/java/com/seliverstov/popularmovies/rest/FullTestSuite.java
@@ -0,0 +1,15 @@
+package com.seliverstov.popularmovies.rest;
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Created by a.g.seliverstov on 05.11.2015.
+ */
+public class FullTestSuite extends TestSuite {
+    public static Test suite(){
+            return new TestSuiteBuilder(FullTestSuite.class).includeAllPackagesUnderHere().build();
+    }
+}
