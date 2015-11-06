7b7fa488da5654e251d8b0a5c75de42dbc108b74
Alexander Seliverstov <seliverstov.a@gmail.com>
2015-11-06 18:28:28 +0300

    Update details view to get data from content provider

--- app/src/main/java/com/seliverstov/popularmovies/MovieDetailsFragment.java
+++ app/src/main/java/com/seliverstov/popularmovies/MovieDetailsFragment.java
@@ -1,6 +1,7 @@
 package com.seliverstov.popularmovies;
 
 import android.app.Fragment;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,49 +10,72 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.seliverstov.popularmovies.rest.model.Movie;
 import com.squareup.picasso.Picasso;
 
 import java.text.DecimalFormat;
+
+import static com.seliverstov.popularmovies.db.PopularMoviesContact.*;
 
 /**
  * Created by a.g.seliverstov on 15.10.2015.
  */
 public class MovieDetailsFragment extends Fragment {
-    public static final String EXTRA_MOVIE_OBJECT = "MOVIE_OBJECT";
     public static final String LOG_TAG = MovieDetailsFragment.class.getSimpleName();
+
+    private static String[] COLUMNS = {
+            MovieEntry.COLUMN_ORIGINAL_TITLE,
+            MovieEntry.COLUMN_RELEASE_DATE,
+            MovieEntry.COLUMN_VOTE_AVERAGE,
+            MovieEntry.COLUMN_OVERVIEW,
+            MovieEntry.COLUMN_POSTER_PATH
+    };
+
+    private static int IDX_ORIGINAL_TITLE = 0;
+    private static int IDX_RELEASE_DATE = 1;
+    private static int IDX_VOTE_AVERAGE = 2;
+    private static int IDX_OVERVIEW = 3;
+    private static int IDX_POSTER_PATH = 4;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_movie_details, container, false);
-        Movie m = (Movie)getActivity().getIntent().getSerializableExtra(EXTRA_MOVIE_OBJECT);
-        TextView title = (TextView)view.findViewById(R.id.movie_title);
-        TextView year = (TextView)view.findViewById(R.id.movie_year);
-        TextView rating = (TextView)view.findViewById(R.id.movie_rating);
-        TextView overview = (TextView)view.findViewById(R.id.movie_overview);
+        Uri uri = getActivity().getIntent().getData();
+        Cursor c = getActivity().getContentResolver().query(uri,COLUMNS,null,null,null);
+        if (c.moveToFirst()){
+            TextView title = (TextView)view.findViewById(R.id.movie_title);
+            TextView year = (TextView)view.findViewById(R.id.movie_year);
+            TextView rating = (TextView)view.findViewById(R.id.movie_rating);
+            TextView overview = (TextView)view.findViewById(R.id.movie_overview);
 
-        title.setText(m.getOriginalTitle());
-        String rd = m.getReleaseDate();
-        rd = (rd !=null && rd.length()>=4)? rd.substring(0,4): "";
+            String originalTitle = c.getString(IDX_ORIGINAL_TITLE);
+            title.setText(originalTitle);
+            String rd = c.getString(IDX_RELEASE_DATE);
+            rd = (rd !=null && rd.length()>=4)? rd.substring(0,4): "";
 
-        year.setText(rd);
-        rating.setText(new DecimalFormat("#.#").format(m.getVoteAverage())+"/10");
-        overview.setText(m.getOverview());
+            year.setText(rd);
+            rating.setText(new DecimalFormat("#.#").format(c.getDouble(IDX_VOTE_AVERAGE))+"/10");
+            overview.setText(c.getString(IDX_OVERVIEW));
 
-        ImageView poster = (ImageView)view.findViewById(R.id.movie_poster);
+            ImageView poster = (ImageView)view.findViewById(R.id.movie_poster);
 
-        if (m.getPosterPath()!=null) {
-            Uri url = Uri.parse(getString(R.string.movie_poster_base_url))
-                    .buildUpon()
-                    .appendPath(getString(R.string.big_movie_poster_size))
-                    .appendEncodedPath(m.getPosterPath())
-                    .build();
-            Log.i(LOG_TAG, "Get poster for movie " + m.getTitle() + ": " + url.toString());
-            Picasso.with(getActivity()).load(url).placeholder(R.drawable.loading_big).into(poster);
+            String posterPath = c.getString(IDX_POSTER_PATH);
+            if (posterPath!=null) {
+                Uri url = Uri.parse(getString(R.string.movie_poster_base_url))
+                        .buildUpon()
+                        .appendPath(getString(R.string.big_movie_poster_size))
+                        .appendEncodedPath(posterPath)
+                        .build();
+                Log.i(LOG_TAG, "Get poster for movie " + originalTitle + ": " + url.toString());
+                Picasso.with(getActivity()).load(url).placeholder(R.drawable.loading_big).into(poster);
+            }else{
+                poster.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                poster.setImageResource(R.drawable.noposter);
+            }
         }else{
-            poster.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            poster.setImageResource(R.drawable.noposter);
+            Toast.makeText(getActivity(),"Can't find movie details :(",Toast.LENGTH_SHORT).show();
         }
+
         return view;
     }
 }
