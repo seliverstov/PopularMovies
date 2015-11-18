31cb08a66b6486b82b95e910eac6741f47767218
Alexander Seliverstov <seliverstov.a@gmail.com>
2015-11-18 14:22:46 +0300

    Refactor DetailsFragment to use Loader

--- app/src/main/java/com/seliverstov/popularmovies/MovieDetailsFragment.java
+++ app/src/main/java/com/seliverstov/popularmovies/MovieDetailsFragment.java
@@ -1,8 +1,11 @@
 package com.seliverstov.popularmovies;
 
 import android.app.Fragment;
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -39,6 +42,9 @@ public class MovieDetailsFragment extends Fragment {
             MovieEntry.COLUMN_FAVORITE
     };
 
+    public static final int CURSOR_MOVIE_DETAILS_LOADER = 2;
+    public static final int TMDB_MOVIE_DETAILS_LOADER = 3;
+
     private static int IDX_ID = 0;
     private static int IDX_ORIGINAL_TITLE = 1;
     private static int IDX_RELEASE_DATE = 2;
@@ -47,6 +53,12 @@ public class MovieDetailsFragment extends Fragment {
     private static int IDX_POSTER_PATH = 5;
     private static int IDX_FAVORITE = 6;
     private Uri mUri;
+    private TextView mTitle;
+    private TextView mYear;
+    private TextView mRating;
+    private TextView mOverview;
+    private ImageView mPoster;
+    private Button mFavorite;
 
     public Uri getMovieUri(){
         return mUri;
@@ -61,70 +73,111 @@ public class MovieDetailsFragment extends Fragment {
         if (mUri==null) return null;
 
         final View view = inflater.inflate(R.layout.fragment_details, container, false);
-        final Cursor c = getActivity().getContentResolver().query(mUri, COLUMNS, null, null, null);
-
-        if (c.moveToFirst()){
-            TextView title = (TextView)view.findViewById(R.id.movie_title);
-            TextView year = (TextView)view.findViewById(R.id.movie_year);
-            TextView rating = (TextView)view.findViewById(R.id.movie_rating);
-            TextView overview = (TextView)view.findViewById(R.id.movie_overview);
-
-            String originalTitle = c.getString(IDX_ORIGINAL_TITLE);
-            title.setText(originalTitle);
-            String rd = c.getString(IDX_RELEASE_DATE);
-            rd = (rd !=null && rd.length()>=4)? rd.substring(0,4): "";
-
-            year.setText(rd);
-            rating.setText(new DecimalFormat("#.#").format(c.getDouble(IDX_VOTE_AVERAGE))+"/10");
-            overview.setText(c.getString(IDX_OVERVIEW));
-
-            ImageView poster = (ImageView)view.findViewById(R.id.movie_poster);
-
-            String posterPath = c.getString(IDX_POSTER_PATH);
-            if (posterPath!=null) {
-                Uri url = Uri.parse(getString(R.string.movie_poster_base_url))
-                        .buildUpon()
-                        .appendPath(getString(R.string.big_movie_poster_size))
-                        .appendEncodedPath(posterPath)
-                        .build();
-                Log.i(LOG_TAG, "Get poster for movie " + originalTitle + ": " + url.toString());
-                Picasso.with(getActivity()).load(url).placeholder(R.drawable.loading_big).into(poster);
-            }else{
-                poster.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                poster.setImageResource(R.drawable.noposter);
-            }
-
-            final Button favorite = (Button)view.findViewById(R.id.favorite);
-            final int f = c.getInt(IDX_FAVORITE);
-            if (f == 0){
-                favorite.setText("Add to Favorite");
-            }else{
-                favorite.setText("Remove from Favorite");
-            }
-            favorite.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    ContentValues cv = new ContentValues();
-                    if (f == 0) {
-                        cv.put(MovieEntry.COLUMN_FAVORITE, 1);
-                        favorite.setText("Remove from Favorite");
-                    }else{
-                        cv.put(MovieEntry.COLUMN_FAVORITE, (Integer) null);
-                        favorite.setText("Add to Favorite");
-                    }
-                    int id = c.getInt(IDX_ID);
-                    long u = getActivity().getContentResolver().update(MovieEntry.CONTENT_URI,cv, MovieEntry._ID+" = ?",new String[]{String.valueOf(id)});
-                    Log.i(LOG_TAG,"Movie "+id+" was updated: "+u);
-                }
-            });
-        }else{
-            Toast.makeText(getActivity(),"Can't find movie details :(",Toast.LENGTH_SHORT).show();
-        }
+        mTitle = (TextView)view.findViewById(R.id.movie_title);
+        mYear = (TextView)view.findViewById(R.id.movie_year);
+        mRating = (TextView)view.findViewById(R.id.movie_rating);
+        mOverview = (TextView)view.findViewById(R.id.movie_overview);
+        mPoster = (ImageView)view.findViewById(R.id.movie_poster);
+        mFavorite = (Button)view.findViewById(R.id.favorite);
 
         return view;
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(CURSOR_MOVIE_DETAILS_LOADER,null,new CursorLoaderCallback()).forceLoad();
+    }
+
     public void onSortOrderChange(){
-        
+
+    }
+
+    class CursorLoaderCallback implements LoaderManager.LoaderCallbacks<Cursor>{
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            if (mUri!=null)
+                return new CursorLoader(getActivity(),mUri,COLUMNS,null,null,null);
+            else
+                return null;
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
+            if (c.moveToFirst()){
+                String originalTitle = c.getString(IDX_ORIGINAL_TITLE);
+                mTitle.setText(originalTitle);
+
+                String rd = c.getString(IDX_RELEASE_DATE);
+                rd = (rd !=null && rd.length()>=4)? rd.substring(0,4): "";
+
+                mYear.setText(rd);
+
+                mRating.setText(new DecimalFormat("#.#").format(c.getDouble(IDX_VOTE_AVERAGE)) + "/10");
+
+                mOverview.setText(c.getString(IDX_OVERVIEW));
+
+                String posterPath = c.getString(IDX_POSTER_PATH);
+                if (posterPath!=null) {
+                    Uri url = Uri.parse(getString(R.string.movie_poster_base_url))
+                            .buildUpon()
+                            .appendPath(getString(R.string.big_movie_poster_size))
+                            .appendEncodedPath(posterPath)
+                            .build();
+                    Log.i(LOG_TAG, "Get poster for movie " + originalTitle + ": " + url.toString());
+                    Picasso.with(getActivity()).load(url).placeholder(R.drawable.loading_big).into(mPoster);
+                }else{
+                    mPoster.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    mPoster.setImageResource(R.drawable.noposter);
+                }
+
+
+                final int f = c.getInt(IDX_FAVORITE);
+                if (f == 0){
+                    mFavorite.setText("Add to Favorite");
+                }else{
+                    mFavorite.setText("Remove from Favorite");
+                }
+                final int id = c.getInt(IDX_ID);
+                mFavorite.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ContentValues cv = new ContentValues();
+                        if (f == 0) {
+                            cv.put(MovieEntry.COLUMN_FAVORITE, 1);
+                            mFavorite.setText("Remove from Favorite");
+                        } else {
+                            cv.put(MovieEntry.COLUMN_FAVORITE, (Integer) null);
+                            mFavorite.setText("Add to Favorite");
+                        }
+                        long u = getActivity().getContentResolver().update(MovieEntry.CONTENT_URI, cv, MovieEntry._ID + " = ?", new String[]{String.valueOf(id)});
+                        Log.i(LOG_TAG, "Movie " + id + " was updated: " + u);
+                    }
+                });
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+
+        }
+    }
+
+    class TMDBLoaderCallback implements LoaderManager.LoaderCallbacks<Void>{
+
+        @Override
+        public Loader<Void> onCreateLoader(int id, Bundle args) {
+            return null;
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Void> loader, Void data) {
+
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Void> loader) {
+
+        }
     }
 }
